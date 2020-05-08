687330548c46bda34e0b4b9cf4df7386a5d48a00
Nina Nazarova <ninanazarova0496@yandex.ru>
2020-05-08 13:49:04 +0300

    add image container

--- scripts/cards.js
+++ scripts/cards.js
@@ -5,6 +5,7 @@ const cardTemplate = document
 
 const addCardOpen = document.querySelector(".user-info__button");
 
+const regexpImageUrl = /^url\(\"(\S+)\"\)/;
 // Создаем попап для карточек
 const addCardPopup = createPopup({
   title: "Новое место",
@@ -46,7 +47,8 @@ function addCardListener(popup) {
   };
 }
 
-// Добавляем карточки из data.js
+// Функция для добавления карточки из data.js
+
 function insertCards(cards) {
   cards.forEach(function (elem) {
     const card = createCard(elem.link, elem.name);
@@ -54,10 +56,16 @@ function insertCards(cards) {
   });
 }
 
+// Функция-обработчик всех кликов по карточке
 function handleCardClick(event) {
   const target = event.target;
   if (target.classList.contains("place-card__like-icon")) {
     target.classList.toggle("place-card__like-icon_liked");
+  }
+  if (target.classList.contains("place-card__image")) {
+    const bg = target.style["background-image"];
+    const link = regexpImageUrl.exec(bg)[1];
+    createImgContainer(link);
   }
   if (target.classList.contains("place-card__delete-icon")) {
     target.closest(".places-list").removeChild(target.closest(".place-card"));
@@ -74,3 +82,23 @@ addCardPopup.form.addEventListener("submit", addCardListener(addCardPopup));
 
 //Добавляем дефолтные карточки
 insertCards(initialCards);
+
+// Открытие картинки
+function createImgContainer(linkValue) {
+  const markup = `
+  <div class="image-popup image-popup_is-opened">
+    <div class="image-popup__container">
+      <img src="${linkValue}" class="image-popup__image" />
+      <img src="./images/close.svg" class="image-popup__close" />
+    </div>
+  </div>
+  `;
+
+  root.insertAdjacentHTML("afterbegin", markup);
+  const imgPopup = root.firstElementChild;
+  const closeButton = document.querySelector(".image-popup__close");
+
+  closeButton.addEventListener("click", () => {
+    imgPopup.classList.toggle("image-popup_is-opened");
+  });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -53,5 +53,3 @@ function createPopup(args) {
     },
   };
 }
-
-// Вызовы функций
