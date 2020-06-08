67722da69d301238d0bcb1a236c974f07ca3a849
Nina Nazarova <ninanazarova0496@yandex.ru>
2020-06-08 16:14:58 +0300

    add server

--- scripts/UserInfo.js
+++ scripts/UserInfo.js
@@ -4,24 +4,20 @@ class UserInfo {
   constructor(name, about) {
     this._name = name;
     this._about = about;
+    this.updateUserInfo();
   }
 
   getUserInfo = () => {
     return { name: this._name, about: this._about };
   };
 
-  setUserInfo = (name, about) => {
+  setUserInfo = ({ name, about }) => {
     this._name = name;
     this._about = about;
   };
 
-  updateUserInfo = (form) => {
-    const nameInput = form.elements.name.value;
-    const aboutInput = form.elements.about.value;
-
-    this.setUserInfo(nameInput, aboutInput);
-
-    document.querySelector(".user-info__name").textContent = nameInput;
-    document.querySelector(".user-info__job").textContent = aboutInput;
+  updateUserInfo = () => {
+    document.querySelector(".user-info__name").textContent = this._name;
+    document.querySelector(".user-info__job").textContent = this._about;
   };
 }
